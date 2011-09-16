8d6a7aed8b22601afd6eb2c797f1c091fb911880
Tok <tok@firstb2b.net>
2011-09-16 12:21:37 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1cf2e038c854e6a44b346db99083964b39a4f3fcb739bddaedfca0ba383d98f8bdfb8c8daebad4befab9c8b9f681dcb91bac56124aba2869d6507fee1edf59ff'
